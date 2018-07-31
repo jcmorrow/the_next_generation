06fd5220cdbb39e8870a94607bb450d47919a4e6
Josh Morrow <josh@jcmorrow.com>
2018-07-31 15:17:45 +0700

    Add decks for players
    And make sure each contains 8 scouts and 2 vipers by default.

--- src/player.rs
+++ src/player.rs
@@ -1,16 +1,30 @@
+use card::Card;
+
 const STARTING_AUTHORITY: i32 = 50;
 
 #[derive(Debug)]
 pub struct Player {
     pub name: String,
-    pub authority: i32
+    pub authority: i32,
+    pub deck: Vec<Card>,
 }
 
 impl Player {
     pub fn new(name: &str) -> Player {
-        Player{
+        let mut player = Player{
             name: name.to_string(),
-            authority: STARTING_AUTHORITY
+            authority: STARTING_AUTHORITY,
+            deck: Vec::new(),
+        };
+
+        for _n in 1..8 {
+            player.deck.push(Card::scout());
         }
+
+        for _n in 1..2 {
+            player.deck.push(Card::viper());
+        }
+
+        return player;
     }
 }
