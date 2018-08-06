ca061a863e77f966a5daa25e0609fe14638d71b1
Cameron McCallie <cam@nomadhealth.com>
2018-08-06 17:41:33 +0700

    Refactor Card to trait

--- src/player.rs
+++ src/player.rs
@@ -1,4 +1,6 @@
 use card::Card;
+use card::ship::Scout;
+
 use std::fmt;
 
 const STARTING_AUTHORITY: i32 = 50;
@@ -6,7 +8,11 @@ const STARTING_AUTHORITY: i32 = 50;
 pub struct Player {
     pub name: String,
     pub authority: i32,
-    pub deck: Vec<Card>,
+    pub trade:  i32,
+    pub combat: i32,
+    pub discard: Vec<Box<Card>>,
+    pub deck: Vec<Box<Card>>,
+    pub played: Vec<Box<Card>>,
 }
 
 impl Player {
@@ -14,15 +20,15 @@ impl Player {
         let mut player = Player{
             name: name.to_string(),
             authority: STARTING_AUTHORITY,
+            trade: 0,
+            combat: 0,
+            discard: Vec::new(),
             deck: Vec::new(),
+            played: Vec::new(),
         };
 
         for _n in 0..8 {
-            player.deck.push(Card::scout());
-        }
-
-        for _n in 0..2 {
-            player.deck.push(Card::viper());
+            player.deck.push(Box::new(Scout::new()));
         }
 
         return player;
@@ -31,12 +37,14 @@ impl Player {
 
 impl fmt::Display for Player {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let deck_string = String::new();
         write!(f, "Name: {}\n", self.name).unwrap();
-        write!(f, "Deck:\n").unwrap();
-        for card in self.deck.iter() {
-            write!(f, "  {}", card).unwrap();
-        }
-        write!(f, "\n")
+        write!(f, "Trade: {}\n", self.trade)
+        // TODO: fix deck logging
+        // write!(f, "Name: {}\n", self.name).unwrap();
+        // write!(f, "Deck:\n").unwrap();
+        // for card in self.deck.iter() {
+        //     write!(f, "  {}", card).unwrap();
+        // }
+        // write!(f, "\n")
     }
 }
