f9f3044ff470105da0f355037d3c01019f20e661
Cameron McCallie <cam@nomadhealth.com>
2018-08-19 15:08:54 +0700

    Play some attackers

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use player::Player;
 use card::ship::Scout;
+use card::ship::Viper;
 use card::Card;
 
 mod card;
@@ -9,10 +10,13 @@ fn main() {
     let mut player_1 = Player::new("Cameron");
 
     let scout = Scout::new();
+    let viper = Viper::new();
 
     print!("{:#}", player_1);
-    player_1 = scout.play(&player_1);
+    player_1 = scout.play(player_1);
+    player_1 = viper.play(player_1);
     print!("{:#}", player_1);
-    player_1 = scout.play(&player_1);
+    player_1 = scout.play(player_1);
+    player_1 = viper.play(player_1);
     print!("{:#}", player_1);
 }
