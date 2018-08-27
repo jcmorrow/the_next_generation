0c5a5c22715d110a1a49c41dca2081ee66cbaeeb
Josh Morrow <josh@jcmorrow.com>
2018-08-27 20:40:10 +0700

    WIP

--- src/card/outpost.rs
+++ src/card/outpost.rs
@@ -1,57 +1,57 @@
-use std::fmt;
+// use std::fmt;
 
-use card::Card;
-use card::CardType;
-use card::Faction;
-use card::targetable::Targetable;
+// use card::Card;
+// use card::CardType;
+// use card::Faction;
+// use card::targetable::Targetable;
 
-use player::Player;
+// use player::Player;
 
-pub struct BattleStation {
-    pub card_type: CardType,
-    pub combat: i32,
-    pub cost: i32,
-    pub faction: Faction,
-    pub health: i32,
-    pub name: String,
-    pub trade: i32
-}
+// pub struct BattleStation {
+//     pub card_type: CardType,
+//     pub combat: i32,
+//     pub cost: i32,
+//     pub faction: Faction,
+//     pub health: i32,
+//     pub name: String,
+//     pub trade: i32
+// }
 
-impl BattleStation {
-    pub fn new() -> BattleStation {
-        BattleStation{
-            card_type: CardType::Outpost,
-            combat: 0,
-            cost: 3,
-            faction: Faction::MachineCult,
-            health: 5,
-            name: String::from("Battle Station"),
-            trade: 0,
-        }
-    }
-}
+// impl BattleStation {
+//     pub fn new() -> BattleStation {
+//         BattleStation{
+//             card_type: CardType::Outpost,
+//             combat: 0,
+//             cost: 3,
+//             faction: Faction::MachineCult,
+//             health: 5,
+//             name: String::from("Battle Station"),
+//             trade: 0,
+//         }
+//     }
+// }
 
-impl Card for BattleStation {
-    fn play(&self, player: &mut Player) {
-        // TODO: figure out how to push self onto player's bases list
-        player.bases.push(Box::new(BattleStation::new()));
-    }
-}
+// impl Card for BattleStation {
+//     fn play(&self, player: &mut Player) {
+//         // TODO: figure out how to push self onto player's bases list
+//         player.bases.push(Box::new(BattleStation::new()));
+//     }
+// }
 
-impl Targetable for BattleStation {
-    fn is_targetable(&self) -> bool {
-        true
-    }
+// impl Targetable for BattleStation {
+//     fn is_targetable(&self) -> bool {
+//         true
+//     }
 
-    fn receive_combat(&mut self, combat: i32) {
-        if combat >= self.health {
-            // TODO: move from player.bases to player.discard
-        }
-    }
-}
+//     fn receive_combat(&mut self, combat: i32) {
+//         if combat >= self.health {
+//             // TODO: move from player.bases to player.discard
+//         }
+//     }
+// }
 
-impl fmt::Display for BattleStation {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "<{}: {}>\n", self.card_type, self.name)
-    }
-}
+// impl fmt::Display for BattleStation {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         write!(f, "<{}: {}>\n", self.card_type, self.name)
+//     }
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 extern crate rand;
 
 use player::Player;
-use trade_row::TradeRow;
 
 mod card;
 mod player;
@@ -9,23 +8,5 @@ mod trade_row;
 
 fn main() {
     let mut player_1 = Player::new("Cameron");
-    let mut player_2 = Player::new("Josh");
-    let mut trade_row = TradeRow::new();
-
-    let mut players = Vec::new();
-    let mut turn_count = 0;
-    players.push(&mut player_1);
-    players.push(&mut player_2);
-
-
-    while !players.iter().any(|ref p| p.authority < 1)
-    {
-        print!("Turn {}\n", turn_count);
-        players.rotate_left(1);
-        let (current_player, opponents)  = &mut players.split_at_mut(1);
-        print!("{:#}", current_player[0]);
-        current_player[0].take_turn(opponents, &mut trade_row);
-        turn_count = turn_count + 1;
-        print!("{:#}", trade_row);
-    }
+    player_1.take_turn();
 }
