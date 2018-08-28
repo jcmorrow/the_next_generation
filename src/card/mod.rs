873cf1aa2df185f094d4d35d29dbcf775400032b
Josh Morrow <josh@jcmorrow.com>
2018-08-28 15:30:22 +0700

    IDK if this is better or not

--- src/card/mod.rs
+++ src/card/mod.rs
@@ -7,37 +7,66 @@ pub mod targetable;
 
 pub enum Faction {
     MachineCult,
-    Unaligned
+    Unaligned,
+}
+
+impl Default for Faction {
+    fn default() -> Faction { Faction::Unaligned }
 }
 
 pub enum CardType {
+    NoCardType,
     Outpost,
-    Ship
+    Ship,
+}
+
+impl Default for CardType {
+    fn default() ->  CardType { CardType::NoCardType }
 }
 
 pub enum ShipType {
     Explorer,
+    NoShipType,
     Scout,
     Viper,
 }
+
+impl Default for ShipType {
+    fn default() ->  ShipType { ShipType::NoShipType }
+}
+
+pub enum OutpostType {
+    BattleStation,
+    NoOutpostType,
+}
+
+impl Default for OutpostType {
+    fn default() ->  OutpostType { OutpostType::NoOutpostType }
+}
+
 
 impl fmt::Display for CardType {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let name = match *self {
             CardType::Ship => "Ship",
-            CardType::Outpost => "Outpost"
+            CardType::Outpost => "Outpost",
+            CardType::NoCardType => "NoCardType",
         };
         write!(f, "{}", name)
     }
 }
 
+
+#[derive(Default)]
 pub struct Card {
     pub card_type: CardType,
     pub combat: i32,
     pub cost: i32,
     pub faction: Faction,
+    pub health: i32,
     pub name: String,
     pub ship_type: ShipType,
+    pub outpost_type: OutpostType,
     pub trade: i32,
 }
 
@@ -45,24 +74,21 @@ impl Card {
     pub fn scout() -> Card {
         Card {
             card_type: CardType::Ship,
-            combat: 0,
-            cost: 0,
-            faction: Faction::Unaligned,
             name: String::from("Scout"),
             trade: 1,
             ship_type: ShipType::Scout,
+            ..Default::default()
         }
     }
 
     pub fn explorer() -> Card {
         Card {
             card_type: CardType::Ship,
-            combat: 0,
             cost: 2,
-            faction: Faction::Unaligned,
             name: String::from("Explorer"),
-            trade: 2,
             ship_type: ShipType::Explorer,
+            trade: 2,
+            ..Default::default()
         }
     }
 
@@ -70,11 +96,21 @@ impl Card {
         Card {
             card_type: CardType::Ship,
             combat: 1,
-            cost: 0,
-            faction: Faction::Unaligned,
             name: String::from("Viper"),
-            trade: 0,
             ship_type: ShipType::Viper,
+            ..Default::default()
+        }
+    }
+
+    pub fn battle_station() -> Card {
+        Card {
+            card_type: CardType::Outpost,
+            cost: 3,
+            faction: Faction::MachineCult,
+            health: 5,
+            name: String::from("Battle Station"),
+            outpost_type: OutpostType::BattleStation,
+            ..Default::default()
         }
     }
 }
